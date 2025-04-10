1354914a2fd3ef102e0f115e2c6b4274624fcfaf
Juan David López <j.lopez18@utp.edu.co>
2025-04-09 19:19:13 -0500

    proyecto final

---
+++ proyectoFinal01/QueriesOnTheQueue.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("Solo quiero aparecer en contribuidores\n");
+    printf("PostData: dejemen este ejercicio pa mi, estoy rabon con este\n");
+
+
+    return 0;
+}
